0bc46dfd208740ac267b2b100481ae1475c06d93
Avi Halachmi (:avih) <avihpit@yahoo.com>
2018-08-14 11:28:28 +0300

    resolve(v): follow if v is a promise, improve executor
    Despite passing all the tests, the static Promise.resolve() was not
    working correctly. However, this was also not tested till now, as the
    test suite focus on .then(), and provide its own static resolve() if one
    isn't available by the module (it wasn't).
    
    This is a refactor of the code to use a single resolve() function which
    (hopefully) performs well now under all cases:
    - Previously the static resolve didn't follow a promise and instead
      incorrectly fulfilled with this promise as the value. Now it follows
      it.
    - The dynamic resolve did mostly follow a promise, but didn't seal our
      fate, e.g. if resolve was called with a delayed promise and then
      fulfilled with an already resolved promise, the latter settled rather
      than a following the first promise and waiting for it to settle. Now
      this works correctly.
    
    Additionaly, the terminology throughout the comments/functions was
    refined, specifically adding the terms "fate" and "settled", which were
    incorporated into the fixed resolve().
    
    resolve/reject are now tested - after adding the names which the test
    suit expects (resolved/rejected).
    
    The modern constructor now returns a rejected promise if the executor
    threw an error (unless its fate was sealed earlier at the executor -
    same as in other compliant implementations).
    
    Additionally, many comments were rewritten/refined.
    
    Performance remains as competitive as before, or even faster.

--- nopromise.js
+++ nopromise.js
@@ -1,10 +1,36 @@
-/* http://github.com/avih/nopromise MIT */
+/*
+ * NoPromise: Promise A+ compliant implementation, also with ES6 interface
+ * Copyright 2016  Avi Halachmi (:avih)  http://github.com/avih/nopromise
+ * License: MIT
+ *
+ * Interface, e.g. after var Promise = require("nopromise") :
+ *   new Promise(function executor(resolveFn, rejectFn) { ... })
+ *   Promise.prototype.then(onFulfilled, onRejected)
+ *   Promise.prototype.catch(onRejected)
+ *   Promise.prototype.finally(onFinally)
+ *   Promise.resolve(value)
+ *   Promise.reject(reason)
+ *   Promise.all(iterator)
+ *   Promise.race(iterator)
+ *
+ * Legacy interface is also supported:
+ *   Promise.defer()  or  Promise.deferred()
+ *   Promise.prototype.resolve(value)
+ *   Promise.prototype.reject(reason)
+ */
+
 (function(G){
 
-// Note: NoPromise[.prototype].resolve() are the only places where "resolve"
-// actually means "fulfill". Everywhere else, and specifically at the comments,
-// "resolved" means "not pending" - which can be either fulfilled or rejected.
-// The variable names also reflect this "resolved" == "not-pending" notion.
+// Promise terminology:
+// State: begins with pending, may move once to fulfilled+value/rejected+reason.
+// Settled: at its final fulfilled/rejected state.
+// Resolved = sealed fate: settled or (pending and) follows another thenable T.
+//   resolved + pending = follows only T - ignores later resolve/reject calls.
+// Thenable = has .then(): promise but not necessarily our implementation.
+// Note: the API has reject(r)/resolve(v) but not fulfill(v). that is because:
+//   resolve(v) already fulfills with value v if v is not a promise.
+//   if v is a promise then it should be followed - not be a fulfilled value.
+//   hence resolve(v) either fulfills with v, or follows + seals the fate to v.
 
 var globalQ,
     async,
@@ -46,8 +72,8 @@ function internalAsync(f) {
     }
 }
 
-// Fulfil/reject a promise if it's pending, else no-op.
-function unpend(p, state, value) {
+// fulfill/reject a promise if it's pending, else no-op.
+function settle(p, state, value) {
     if (!p._state) {
         p._state = state;
         p._output = value;
@@ -64,10 +90,53 @@ function unpend(p, state, value) {
     }
 }
 
+function reject(promise, reason) {
+    promise._sealed || promise._state || settle(promise, REJECTED, reason);
+}
+
+// a promise's fate may be set only once. fullfill/reject trivially set its fate
+// (and also settle it), but resolving it with another promise P must also seal
+// its fate, such that no later fulfill/reject/resolve are allowed to affect its
+// fate - onlt P will do so once/if it settles.
+// promise here is always NoPromise, but x might be a value/NoPromise/thenable.
+function resolve(promise, x, decidingFate) {
+    if (promise._state || (promise._sealed && !decidingFate))
+        return;
+    // seal fate. only this instance of resolve can settle it [recursively]
+    promise._sealed = 1;
+
+    var then,
+        done = 0;
+
+    try {
+        if (x == promise) {
+            settle(promise, REJECTED, TypeError());
+
+        } else if (x instanceof NoPromise && x._state) {
+            // we can settle synchronously if we know that x is settled and also
+            // know how to adopt its state, which we do when x is NoPromise.
+            settle(promise, x._state, x._output);
+
+        // Check for generic thenable... which includes unsettled NoPromise.
+        } else if ((x && typeof x == "object" || typeof x == FUNCTION)
+                   && typeof (then = x.then) == FUNCTION) {
+            then.call(x, function(y) { done++ || resolve(promise, y, 1) },  // decidingFate
+                         function(r) { done++ || settle(promise, REJECTED, r)});
+
+        } else {
+            settle(promise, FULFILLED, x);
+        }
+
+    } catch (e) {
+        done++ || settle(promise, REJECTED, e);
+    }
+}
+
 // Other than the prototype methods, the object may also have:
 // ._state    : 1 if fulfilled, 2 if rejected (doesn't exist otherwise).
 // ._output   : value if fulfilled, reason if rejected (doesn't exist otherwise).
 // ._resolvers: array of functions (closures) for each .then call while pending (if there were any).
+// ._sealed   : new resolve/reject are ignored (exists if resolve was called).
 
 NoPromise.prototype = {
     // Each call to `then` returns a new NoPromise object and creates a closure
@@ -76,59 +145,40 @@ NoPromise.prototype = {
         var _self    = this,
             promise2 = new NoPromise;
 
-        _self._state ? internalAsync(promise2Resolver)
-                     : _self._resolvers ? _self._resolvers.push(promise2Resolver)
-                                        : _self._resolvers = [promise2Resolver];
+        this._state ? internalAsync(promise2Resolver)
+                    : this._resolvers ? this._resolvers.push(promise2Resolver)
+                                      : this._resolvers = [promise2Resolver];
         return promise2;
 
-        // Invoked asynchronously to `then` and after _self is fulfilled/rejected.
+        // Invoked asynchronously to `then` and after _self is settled.
         // _self._state here is FULFILLED/REJECTED
         function promise2Resolver() {
             var handler = _self._state == FULFILLED ? onFulfilled : onRejected;
 
+            // no executor for promise2, so not yet sealed, but the legacy API
+            // can make it already sealed here, e.g. p2=p.then(); p2.resolve(X)
+            // So we still need to check ._sealed before settle(..) below
             if (typeof handler != FUNCTION) {
-                unpend(promise2, _self._state, _self._output);
+                promise2._sealed || settle(promise2, _self._state, _self._output);
             } else {
-                promise2Resolution(0, 1);
-            }
-
-            function promise2Resolution(x, isFirstTime) {
-                var then,
-                    done = 0;
-
                 try {
-                    if (isFirstTime)
-                        x = handler(_self._output);
-
-                    if (x == promise2) {
-                        unpend(promise2, REJECTED, TypeError());
-
-                    // Check for generic thenable... which includes NoPromise.
-                    } else if ((x && typeof x == "object" || typeof x == FUNCTION)
-                               && typeof (then = x.then) == FUNCTION) {
-                        then.call(x, function(y) { done++ || promise2Resolution(y) },
-                                     function(r) { done++ || unpend(promise2, REJECTED, r)});
-
-                    } else {
-                        unpend(promise2, FULFILLED, x);
-                    }
-
+                    resolve(promise2, handler(_self._output));
                 } catch (e) {
-                    done++ || unpend(promise2, REJECTED, e);
+                    reject(promise2, e);
                 }
             }
         }
-    },
+    },  // then
 
     catch: function(onRejected) {
         return this.then(undefined, onRejected);
     },
 
-    // P.finaly(fn) returns a promise X, and calls fn after P resolves to T.
+    // P.finaly(fn) returns a promise X, and calls fn after P settles as S.
     // if fn throws E: X is rejected with E.
-    // Else if fn returns a promise F: X is resolved once F is resolved:
+    // Else if fn returns a promise F: X is settled once F is settled:
     //   If F is rejected with FJ: X is rejected with FJ.
-    // Else: X mirrors T [ignoring fn's retval or F's fulfillment value]
+    // Else: X settles as S [ignoring fn's retval or F's fulfillment value]
     finally: function(onFinally) {
         function fin_noargs() { return onFinally() }
         var fn;
@@ -142,33 +192,37 @@ NoPromise.prototype = {
 
 
 // CTOR:
-//   return new NoPromise(function(fulfill, reject) { setTimeout(function() { fulfill(42); }, 100); });
+//   return new NoPromise(function(resolve, reject) { setTimeout(function() { resolve(42); }, 100); });
 function NoPromise(executor) {
-    var self = this;
     if (executor) {  // not used inside 'then' nor by the legacy interface
-        executor(function(v) { unpend(self, FULFILLED, v); }, // bind is slower
-                 function(r) { unpend(self, REJECTED,  r); });
+        var self = this;
+        try {
+            executor(function(v) { resolve(self, v) },
+                     function(r) { reject(self, r) });
+        } catch (e) {
+            reject(self, e);
+        }
     }
 }
 
 
-// Static methods
-// --------------
-
-// Returns an already fulfilled/rejected promise with specified value/reason
-NoPromise.resolve = function(v) {
-    return new NoPromise(function(ful, rej) { ful(v); });
-};
-
-NoPromise.reject  = function(r) {
-    return new NoPromise(function(ful, rej) { rej(r); });
-};
-
-// Duplicates the logic used in NoPromise to detect a generic thenable
-function is_promise(x) {
+// detect a generic thenable - same logic as at resolve(). may throw.
+function is_thenable(x) {
     return ((x && typeof x == "object") || typeof x == FUNCTION)
            && typeof x.then == FUNCTION;
 }
+
+// Static methods
+// --------------
+
+// Returns a resolved/rejected promise with specified value(or promise)/reason
+NoPromise.resolve = function(v) {
+    return new NoPromise(function(res, rej) { res(v) });
+};
+
+NoPromise.reject = function(r) {
+    return new NoPromise(function(res, rej) { rej(r) });
+};
 
 
 // For .all and .race: we support iterators as array or array-like, and slack
@@ -190,7 +244,7 @@ NoPromise.all = function(iter) {
         function fulOne(i, val) { rv[i] = val; --pending || allful(rv); }
 
         iter.forEach(function(v, i) {
-            if (is_promise(v)) {
+            if (is_thenable(v)) {
                 pending++;
                 v.then(fulOne.bind(null, i), allrej);
             } else {
@@ -205,14 +259,14 @@ NoPromise.all = function(iter) {
 }
 
 // Static NoPromise.race(iter) returns a promise X:
-// If iter is empty: X never resolves.
-// Else: X resolves always a-sync to mirror the first promise in iter which resolves.
+// If iter is empty: X never settles.
+// Else: X settles always a-sync and mirrors the first promise in iter which settles.
 // (non-promise values are considered already fulfilled with that value).
 NoPromise.race = function(iter) {
     return new NoPromise(function(allful, allrej) {
         Array.isArray(iter) || (iter = [].slice.call(iter));
         iter.some(function(v, i) {
-            if (is_promise(v)) {
+            if (is_thenable(v)) {
                 v.then(allful, allrej);
             } else {
                 NoPromise.resolve(v).then(allful);
@@ -223,25 +277,32 @@ NoPromise.race = function(iter) {
 }
 
 
-// Legacy interface - not used by any NoPromise code, provided as legacy:
+// Legacy interface - not used elsewhere in NoPromise, used by the test suit (below)
 //   var d = NoPromise.defer(); setTimeout(function() { d.resolve(42); }, 100); return d.promise;
-// d.promise is indistinguishable from a promise created as "new NoPromise(executor)"
-NoPromise.defer = NoPromise.deferred = function() {
+NoPromise.defer = function() {
     var d = new NoPromise;
     return d.promise = d;
 };
 
 NoPromise.prototype.resolve = function(value) {
-    unpend(this, FULFILLED, value);
+    resolve(this, value);
 }
 
 NoPromise.prototype.reject = function(reason) {
-    unpend(this, REJECTED, reason);
+    reject(this, reason);
 }
 // End of legacy interface
 
+// Promises/A+ Compliance Test Suite
+// https://github.com/promises-aplus/promises-tests
+// nopromise.js itself can be the adapter: promises-aplus-tests ./nopromise.js
+// The only required modification is that it wants different names - dup below.
+NoPromise.deferred = NoPromise.defer;    // Static legacy API
+NoPromise.resolved = NoPromise.resolve;  // Static standard, optional but tested
+NoPromise.rejected = NoPromise.reject;   // Static standard, optional but tested
+// The tests also use the legacy dynamic API: prototype.resolve(v)/.reject(r)
 
-// export/set-global
+
 try {
     module.exports = NoPromise;
 } catch (e) {
